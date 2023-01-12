5ffde20e9387f5e2aa47a5213124b582545c253b
byxz885@gmail.com <53200767+byzESC@users.noreply.github.com>
2023-01-11 23:15:00 -0800

    fixed all bugs

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
-# 06-Weather-Dashboard
+### 06-Weather-Dashboard
+
+## Description 
+
+    This simple calender application allows user to save their schedule for each hour of the day. 
+    
+    It dynamically updated the HTML to show searched history, and could be use to search for that city's weather again. 
+
+
+Github Repo: https://github.com/byxzESC/05-Work-Day-Scheduler
+
+Deployed Page: https://byxzesc.github.io/05-Work-Day-Scheduler/
+
+> Schedules are being stored in your local Storage
+![example](./assets/images/sample.JPG)
+
+## Table of Contents 
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+
+
+## Installation
+    N/A
+
+
+## Usage 
+
+Step 1. Type in schedule in the text area 
+
+Step 2. click the blue save button on the right
+
+Step 3. The description for the schedule will be different color according to the current time. Gray color indicate the time has already pass, red color indicate its the present, and green is future.
+
+> Date and time provided by [Day.js](https://day.js.org/en/)
+
+![current Time](./assets/images/time_sample.JPG)
+
+> Could be test it by changing the value of currentHour
+
+![testing](./assets/images/currentHour.JPG)
+
+## Credits
+
+N/A
+
+
+## License
+
+N/A
+
+
+## Badges
+
+![badmath](https://img.shields.io/badge/HTML-10%25-orange)
+![badmath](https://img.shields.io/badge/CSS-10%25-blue)
+![badmath](https://img.shields.io/badge/jQuery-10%25-blue)
+![badmath](https://img.shields.io/badge/Javascript-300%25-yellow)
+
+## Features
+
+N/A 
+
+## Tests
+
+N/A
+No written test yet.
--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,24 @@
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
-
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
-
-// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=cbcfd07d3a67829365edbdfcf51d614d
-// API key - cbcfd07d3a67829365edbdfcf51d614d
-// api.openweathermap.org/data/2.5/forecast?q={city name}&appid=cbcfd07d3a67829365edbdfcf51d614d
-
-var sideBar = document.getElementById('side-bar');
+var sideBarEl = document.getElementById('side-bar');
 var searchInput = document.getElementById('side-bar-search');
+var currentWeatherEl = document.getElementById('current-weather');
+var forecastWeatherEl = document.getElementById('forecast-weather');
+var forecastTitleEl = document.getElementById('forecast-title');
 
 var getCityWeather = function (city) {
+    currentWeatherEl.innerHTML = "";
+    forecastWeatherEl.innerHTML = "";
+    
     var apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=cbcfd07d3a67829365edbdfcf51d614d';
     var apiURLForecast = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&appid=cbcfd07d3a67829365edbdfcf51d614d';
-    
-    // console.log(apiURL);
-    fetch(apiURL) 
+    fetch(apiURL)
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
-                renderWeather(data, city);
+                    var temperature = data.main.temp;
+                    var wind = data.wind.speed;
+                    var humidity = data.main.humidity;
+                    var weatherIconURL = 'https://openweathermap.org/img/wn/' + data.weather[0].icon + '.png';
+                    renderWeather(temperature, wind, humidity, weatherIconURL, city);
                 });
             } else {
                 alert('Error: ' + response.statusText);
@@ -36,12 +27,12 @@ var getCityWeather = function (city) {
         .catch(function (error) {
             alert('unable to retrieve weather status');
         });
-    
-    fetch(apiURLForecast) 
+
+    fetch(apiURLForecast)
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
-                renderForecast(data, city);
+                    renderForecast(data.list, city);
                 });
             } else {
                 alert('Error: ' + response.statusText);
@@ -50,27 +41,124 @@ var getCityWeather = function (city) {
         .catch(function (error) {
             alert('unable to retrieve forecast');
         });
+
+
 }
 
-    // data needed from api call are data.name, data.weather[0].main, data.weather[0].icon, data.main.temp, data.main.humidity.
-var renderWeather = function (data, city) {
-    // 
+// Acceptance Criteria --- view current weather conditions for that city
+// presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+var renderWeather = function (temperature, wind, humidity, weatherIconURL, city) {
+    var currentWeatherBlock = document.createElement('div');
+
+    // City name
+    var titleEl = document.createElement('h2');
+    titleEl.textContent = city;
+
+    // weather ICON
+    var weatherIconEl = document.createElement('img');
+    weatherIconEl.setAttribute('src', weatherIconURL);
+
+    // weather details
+    var contentUl = document.createElement('ul');
+    var tempLi = document.createElement('li');
+    var windLi = document.createElement('li');
+    var humidityLi = document.createElement('li');
+    tempLi.textContent = 'Temp: ' + temperature + '°F\n';
+    windLi.textContent = 'Wind: ' + wind + ' MPH\n';
+    humidityLi.textContent = 'Humidity: ' + humidity + '%';
+    contentUl.appendChild(weatherIconEl);
+    contentUl.appendChild(tempLi);
+    contentUl.appendChild(windLi);
+    contentUl.appendChild(humidityLi);
+
+
+    currentWeatherBlock.appendChild(titleEl);
+    titleEl.appendChild(weatherIconEl);
+
+    currentWeatherBlock.appendChild(contentUl);
+    currentWeatherEl.append(currentWeatherBlock);
 }
 
-var renderForecast = function (data, city) {
+// Acceptance Criteria --- view future weather conditions for that city
+// presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+var renderForecast = function (data) {
+    forecastTitleEl.style.display = 'flex';
+    
+    for (var i = 0; i < data.length; i++) {
+        var timeHolder = data[i].dt_txt.split(" ", 2)[1]
+        if (timeHolder === "12:00:00") {
 
+            var forecastBlock = document.createElement('div');
+            forecastBlock.classList.add('forecast-box');
+            // date of the forecast
+            var dateEl = document.createElement('h4');
+            dateEl.textContent = data[i].dt_txt.split(" ", 2)[0];
+
+            // weather ICON
+            var weatherIconURL = 'https://openweathermap.org/img/wn/' + data[i].weather[0].icon + '.png';
+            var weatherIconEl = document.createElement('img');
+            weatherIconEl.setAttribute('src', weatherIconURL);
+
+            // weather forecast detail
+            var temperature = data[i].main.temp;
+            var wind = data[i].wind.speed;
+            var humidity = data[i].main.humidity;
+            var contentUl = document.createElement('ul');
+            var tempLi = document.createElement('li');
+            var windLi = document.createElement('li');
+            var humidityLi = document.createElement('li');
+            tempLi.textContent = 'Temp: ' + temperature + '°F\n';
+            windLi.textContent = 'Wind: ' + wind + ' MPH\n';
+            humidityLi.textContent = 'Humidity: ' + humidity + '%';
+
+            forecastBlock.appendChild(dateEl);
+            contentUl.appendChild(weatherIconEl);
+            contentUl.appendChild(tempLi);
+            contentUl.appendChild(windLi);
+            contentUl.appendChild(humidityLi);
+            forecastBlock.appendChild(contentUl);
+
+            forecastWeatherEl.append(forecastBlock);
+        }
+    }
+}
+
+// Acceptance Criteria --- click on a city in the search history, 
+// presented with current and future conditions for that city
+// render history search buttons
+var previousSearch = function (city)  {
+    var searchBtn = document.createElement('button');
+    searchBtn.textContent = city;
+    searchBtn.classList.add("btn", "btn-secondary", "w-100", "py-2", "my-2")
+
+    searchBtn.addEventListener('click', function(event) {
+        event.preventDefault();
+    
+        console.log(event.target.textContent);
+        var city = event.target.textContent;
+
+        if (city) {
+            getCityWeather(city);
+        }
+    });
+
+    sideBarEl.appendChild(searchBtn);
 }
 
 // listen to user input
-searchInput.addEventListener('submit', function(event) {
+searchInput.addEventListener('submit', function (event) {
     event.preventDefault();
 
     var city = event.target[0].value.trim();
 
     if (city) {
-        getCityWeather(city)
+        previousSearch(city);
+        getCityWeather(city);
         document.getElementById('city-input').textContent = '';
     } else {
         alert('Please enter a city name');
     }
+
+    event.target[0].value = "";
 });
+
